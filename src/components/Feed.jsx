c642b1710ae30c9b3456e8de68f01cda425c4ff1
Shareef Rossetti <shareefrossetti@gmail.com>
2023-07-06 20:44:44 -0400

    starter

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -19,9 +19,9 @@ const Feed = () => {
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-        
+
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 JSM Media
+          Copyright © 2023 Shareef Rossetti
         </Typography>
       </Box>
 
